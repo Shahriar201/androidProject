581eecf240fc0b9cdc23d1ccf30039216a69126f
shahriar201 <shahriarbrine201@gmail.com>
2021-03-25 00:27:08 +0600

    Converted any website to an app

---
+++ DIU/app/src/main/java/com/example/diu/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.diu;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webViewId);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        //for all activity run this app
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://daffodilvarsity.edu.bd/");
+    }
+}
