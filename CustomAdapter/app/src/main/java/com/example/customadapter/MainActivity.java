422a94796ae6c9b4ce5c2a4cd21cb65ed993884d
shahriar201 <shahriarbrine201@gmail.com>
2021-04-06 12:16:18 +0600

    Menu Item and Custom Adapter

---
+++ CustomAdapter/app/src/main/java/com/example/customadapter/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.customadapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+    private String[] countryNames;
+    int[] flags = {
+            R.drawable.bd_icon,
+            R.drawable.india_icon,
+            R.drawable.pakistan_icon
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listViewId);
+
+        CustomAdapter adapter = new CustomAdapter(this, countryNames, flags);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String value = countryNames[position];
+                Toast.makeText(MainActivity.this, value, Toast.LENGTH_SHORT).show();
+            }
+        }); 
+    }
+}
